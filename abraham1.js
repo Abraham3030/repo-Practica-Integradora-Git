6b34120237ac6aad69daf95ae531e35a73eec0f6
Abraham_Montelongo <mocapunk@hotmail.com>
2021-05-10 23:05:56 -0500

    mpaso 7 y 8

--- abraham1.js
+++ abraham1.js
@@ -1,1 +1,4 @@
 console.log("modificacion de los archivos CesarV1");
+
+console.log('Excelente ya salio la practica integradora, yo creo que no hace falta realizar el conflicto');
+//pero tu dime si lo hacemos, esto solo es cuestion de ponerse de acuerdo para que no halla problemas.
